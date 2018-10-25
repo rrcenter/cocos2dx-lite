218ab287cd7069916a0a986b2385fb8171122fc2
yinjimmy <jimmy.yin5@gmail.com>
2018-10-26 00:30:30 +0800

    upgrade to 3.17

--- clean.sh
+++ clean.sh
@@ -3,4 +3,4 @@ rm -fr `find . -name "*win10*"`
 rm -fr `find . -name "*winrt*"`
 rm -fr `find . -name "*win8*"`
 rm -fr `find . -name "*wp_8.1*"`
-rm -fr `find . -name "CMakeLists.txt"`
+#rm -fr `find . -name "CMakeLists.txt"`
