640c1489ac2e21c77e373b89fc46c80e520c448a
yinjimmy <jimmy.yin5@gmail.com>
2019-02-28 23:15:48 +0800

    debug ui

--- src/main.lua
+++ src/main.lua
@@ -3,12 +3,20 @@ package.path = '?.lua;src/?.lua;src/packages/?.lua'
 local fs = cc.FileUtils:getInstance()
 fs:setPopupNotify(false)
 
-local STP = require "StackTracePlus"
-local _traceback = debug.traceback
-debug.traceback = STP.stacktrace
+-- local STP = require "StackTracePlus"
+-- local _traceback = debug.traceback
+-- debug.traceback = STP.stacktrace
 
 local winsize = cc.Director:getInstance():getWinSize()
 local target = cc.Application:getInstance():getTargetPlatform()
+
+function ismobile(  )
+    return (target == 4) or (target == 5)
+end
+function isdesktop(  )
+    return (target == 2) or (target == 3)
+end
+
 __G__TRACKBACK__ = function ( msg )
 
     local message = msg
@@ -16,46 +24,10 @@ __G__TRACKBACK__ = function ( msg )
     print(msg)
 
     -- report lua exception
-    if target == 4 or target == 5 then -- ios
+    if ismobile() then -- ios
         -- buglyReportLuaException(tostring(message), _traceback())
-    elseif target == 2 then -- mac
-        local indx = 2
-        local info = debug.getinfo(indx)
-
-        while info do
-            if info.what == 'Lua' then break end
-            indx = indx + 1
-            info = debug.getinfo(indx)
-        end
-
-        local infoLabel = cc.Label:createWithSystemFont(message, 'sans', 32)
-        infoLabel:setWidth(winsize.width)
-        infoLabel:setAnchorPoint({x=0,y=1})
-        infoLabel:setPosition(0, winsize.height)
-        infoLabel:setTextColor({r=255,g=0,b=0,a=255})
-
-        local scene = cc.Director:getInstance():getRunningScene()
-        if not scene then
-            scene = cc.Scene:create()
-            cc.Director:getInstance():runWithScene(scene)
-        end
-        scene:addChild(infoLabel, 998)
-
-        local function onTouchBegan(touch, event)
-            return true
-        end
-        local function onTouchEnded(touch, event)
-            -- use 'Subline Text' to edit the file
-            local cmd = string.format('subl %s:%d', cc.FileUtils:getInstance():fullPathForFilename(info.source), info.currentline)
-            os.execute(cmd)
-            infoLabel:removeSelf()
-        end
-
-        local listener = cc.EventListenerTouchOneByOne:create()
-        listener:registerScriptHandler(onTouchBegan,40)--cc.Handler.EVENT_TOUCH_BEGAN
-        listener:registerScriptHandler(onTouchEnded,42)--cc.Handler.EVENT_TOUCH_ENDED
-        local eventDispatcher = infoLabel:getEventDispatcher()
-        eventDispatcher:addEventListenerWithSceneGraphPriority(listener, infoLabel)
+    else -- desktop
+        require('ccdebug')()
     end
 
     return msg
---
+++ src/packages/ccdebug.lua
@@ -0,0 +1,79 @@
+
+local winsize = cc.Director:getInstance():getWinSize()
+local target = cc.Application:getInstance():getTargetPlatform()
+local fs = cc.FileUtils:getInstance()
+local director = cc.Director:getInstance()
+local scheduler = director:getScheduler()
+
+
+local win,mac = 0,2
+local KEY_ESC = 6
+local isdesktop = (target <= 2)
+local handler = nil
+
+local editor = '"/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"'
+if target == win then
+    -- editor = ''
+end
+
+local function showdebugui( )
+    local index = 2 -- 2
+    local info = debug.getinfo(index)
+    local lines = {}
+    while info do
+        if info.what == 'Lua' then
+            table.insert(lines, info)
+        end
+        index = index + 1
+        info = debug.getinfo(index)
+    end
+
+    handler = scheduler:scheduleScriptFunc(function ( ... )
+        scheduler:unscheduleScriptEntry(handler)
+        print('>>scheduleScriptFunc')
+        handler = nil
+
+        local scene = director:getRunningScene()
+        if not scene then
+            scene = cc.Scene:create()
+            scene:runWithScene(scene)
+        end
+
+        local node = scene:getChildByTag(999)
+        if node == nil then
+            node = cc.CSLoader:createNode('res/views/debugView.csb')
+            node:setPosition(winsize.width*0.5, winsize.height*0.5)
+            local list = node:getChildByName('panel')
+            list:onClick(function (  )
+                node:hide()
+            end)
+            list:setItemModel(list:getItem(0))
+
+            local listener = cc.EventListenerKeyboard:create()
+            listener:registerScriptHandler(function(keyCode)
+                if isdesktop and (keyCode == KEY_ESC) then
+                    node:setVisible(not node:isVisible())
+                end
+            end, cc.Handler.EVENT_KEYBOARD_RELEASED)
+            local dispatcher = cc.Director:getInstance():getEventDispatcher()
+            dispatcher:addEventListenerWithFixedPriority(listener, -1)
+        end
+        node:show()
+        local list = node:getChildByName('panel')
+        list:removeAllItems()
+        for i,v in ipairs(lines) do
+            list:pushBackDefaultItem()
+            local item = list:getItem(i-1)
+            item:setString(string.format('%s:+%d', v.source:sub(v.source:len()-60), v.currentline))
+            item:onClick(function (  )
+                local cmd = string.format('%s %s:%d', editor, fs:fullPathForFilename(v.source), v.currentline)
+                os.execute(cmd)
+            end)
+        end
+        scene:addChild(node, 999, 999)
+
+    end, 0, false)
+
+end
+
+return showdebugui
